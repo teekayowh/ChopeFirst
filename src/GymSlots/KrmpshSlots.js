caca7717b1b60fed2667b42fbe557b7d3809f6f1
SpecOps2016 <gregory.wong@ymail.com>
2022-06-23 22:04:03 +0800

    updatev2

--- src/GymSlots/KrmpshSlots.js
+++ src/GymSlots/KrmpshSlots.js
@@ -1,19 +1,23 @@
 import "./KrmpshSlots.css";
 import ReactTimeslotCalendar from "react-timeslot-calendar";
 import moment from "moment";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import {
-  getTimeslots
+  getTimeslots, updateTimeslots
 } from '../api/timeslots'
 import{
   createBookings
 } from '../api/bookings'
 import { useState, useEffect } from "react";
 
+
+
 function KrmpshSlots() {
   const [timeslots, setTimeslots] = useState([])
 
   useEffect(() => {
-    getTimeslots().then((slots) => {
+    getTimeslots("krmpsh").then((slots) => {
       var temp = []
       for (var slot in slots) {
         if (slots[slot]['capacity'] > 0) {
@@ -25,14 +29,28 @@ function KrmpshSlots() {
     })
   }, [])
 
+const [user] = useAuthState(auth);
+ 
+const [startDate, setStart] = useState(0);
+const [endDate, setEnd] = useState(0);
+const [startTime, setStarttime] = useState(0);
+const [endTime, setEndtime] = useState(0);
+
   function handleSubmit() {
-    createBookings('test', 'testVenue', {'start': '7', 'end': '9'})
+    createBookings(user.uid, "Krmpsh" , {'start': startDate, 'end': endDate})
+    updateTimeslots("krmpsh", startTime);
+    alert("Booking has been made")
+  }
+
+  function takeNote(s ,e, d, f) {
+    setStart({ startDate: s });
+    setEnd({ endDate: e });
+    setStarttime({startTime: d});
+    setEndtime({endTime: f});
   }
 
   return (
     <div className="KrmpshSlots">
-
-      <button onClick={handleSubmit}>Book</button>
       <h1>Kent Ridge MPSH Gym</h1>
       <h2>Operating Hours: 7am - 9pm</h2>
       <ReactTimeslotCalendar
@@ -40,10 +58,15 @@ function KrmpshSlots() {
         let
         timeslots={timeslots} 
         onSelectTimeslot = {(allTimeslots, lastSelectedTimeslot) => {
-          console.log(lastSelectedTimeslot.startDate);  // MomentJS object.
- 
+          takeNote(lastSelectedTimeslot.startDate.toDate(), lastSelectedTimeslot.endDate.toDate(), lastSelectedTimeslot.startDate.hour(), 
+          lastSelectedTimeslot.endDate.hour());// MomentJS object.
         }}
       />
+
+      <div class="mt-md-4 text-center">
+        <button class="btn btn-primary text-uppercase fw-bold" onClick={handleSubmit} >Submit
+        </button>
+      </div>
     </div>
   );
 }
